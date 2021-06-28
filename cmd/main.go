e0a702c09d238cde1c60d5dd348e88979c85c087
Denis Sipakov <mc.flyer@gmail.com>
2021-06-28 16:44:00 +0300

    Connect to db

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,42 @@
 package main
 
-import "net/http"
-import "log"
+import (
+  "net/http"
+  "log"
+  "github.com/joho/godotenv"
+  "os"
+  "msgo-account/pkg/repository"
+)
 // import "msgo-account/api/server"
 
 func main() {
   // router := server.Init()
 
+  if err := godotenv.Load(); err != nil {
+    log.Fatalf("Cannot open dotenv file: %s", err.Error())
+  }
+
+  db, err := repository.NewPostgresDB(repository.Config{
+    Host: os.Getenv("DB_HOST"),
+    Port: os.Getenv("DB_PORT"),
+    Username: os.Getenv("DB_USER"),
+    Password: os.Getenv("DB_PASSWORD"),
+    DBName: os.Getenv("DB_NAME"),
+    SSLMode: os.Getenv("DB_SSL_MODE"),
+  })
+
+  if err != nil {
+    log.Fatalf(err.Error())
+  }
+
+  db.Ping()
   http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
     log.Println("Index page")
   })
 
-  err := http.ListenAndServe(":9091", nil)
+  err1 := http.ListenAndServe(":9091", nil)
 
-  if (err != nil) {
-    log.Println(err)
+  if (err1 != nil) {
+    log.Println(err1)
   }
 }
--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -15,16 +15,16 @@ type Config struct {
 }
 
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
-  db, err := sqlx.Open("postgres", fmt.Stringf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+  db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
     cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
 
-  if eff != nil {
+  if err != nil {
     return nil, err
   }
 
-  err := db.Ping()
-  if err != nil {
-    return nil, err
+  err1 := db.Ping()
+  if err1 != nil {
+    return nil, err1
   }
 
   return db, nil
