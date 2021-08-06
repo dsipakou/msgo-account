fd6b7aaf0510c6231f8196d6dcd06bda6762253d
Denis Sipakov <mc.flyer@gmail.com>
2021-08-06 12:47:48 +0300

    Fix null currency

--- pkg/db/models/transaction.go
+++ pkg/db/models/transaction.go
@@ -1,16 +1,19 @@
 package models
 
+import "database/sql"
+
 type Transaction struct {
-	Id              int32   `db:"id"`
-	UserId          int32   `db:"user_id"`
-	CategoryId      int32   `db:"category_id"`
-	AccountId       int32   `db:"account_id"`
-	Amount          float32 `db:"amount"`
-	TransactionDate string  `db:"transaction_date"`
-	Type            string  `db:"type"`
-	Description     string  `db:"description"`
-	CreatedAt       string  `db:"created_at"`
-	UpdatedAt       string  `db:"updated_at"`
+	Id              int32         `db:"id"`
+	UserId          int32         `db:"user_id"`
+	CategoryId      int32         `db:"category_id"`
+	AccountId       int32         `db:"account_id"`
+	CurrencyId      sql.NullInt32 `db:"currency_id"`
+	Amount          float32       `db:"amount"`
+	TransactionDate string        `db:"transaction_date"`
+	Type            string        `db:"type"`
+	Description     string        `db:"description"`
+	CreatedAt       string        `db:"created_at"`
+	UpdatedAt       string        `db:"updated_at"`
 }
 
 type JsonTransactionCreate struct {
@@ -18,6 +21,7 @@ type JsonTransactionCreate struct {
 	CategoryId      int32   `json:"categoryId"`
 	Amount          float32 `json:"amount,string,omitempty"`
 	AccountId       int32   `json:"accountId"`
+	CurrencyId      int32   `json:"currencyId"`
 	TransactionDate string  `json:"transactionDate"`
 	Type            string  `json:"type"`
 	Description     string  `json:"description"`
@@ -29,9 +33,10 @@ type JsonTransactionUpdate struct {
 	CategoryId      int32   `json:"categoryId"`
 	Amount          float32 `json:"amount,string,omitempty"`
 	AccountId       int32   `json:"accountId"`
+	CurrencyId      int32   `json:"currencyId"`
 	TransactionDate string  `json:"transactionDate"`
-	Description     string  `json:"description"`
 	Type            string  `json:"type"`
+	Description     string  `json:"description"`
 }
 
 type JsonTransactionDelete struct {
@@ -43,10 +48,11 @@ type JsonTransactionResponse struct {
 	UserId          int32   `json:"userId"`
 	CategoryId      int32   `json:"categoryId"`
 	AccountId       int32   `json:"accountId"`
+	Currency        int32   `json:"currencyId"`
 	Amount          float32 `json:"amount"`
 	TransactionDate string  `json:"transactionDate"`
-	Description     string  `json:"description"`
 	Type            string  `json:"type"`
+	Description     string  `json:"description"`
 	CreatedAt       string  `json:"createdAt"`
 	UpdatedAt       string  `json:"updatedAt"`
 }
